e35361f417d9288941405086b8c982705a25d22b
busuncle <lizhij2@gmail.com>
2013-04-13 21:44:42 +0800

    remove useless methods

--- achievement.py
+++ achievement.py
@@ -40,25 +40,5 @@ class Achievement(object):
             self.hero.attacker.kill_record = []
 
 
-    def record_n_kill(self, enemy_id):
-        self.has_killed.add(enemy_id)
-        kill_time = time.time()
-        # kill_time should be a timestamp
-        self.kill_time_list.append(kill_time)
-
-
-    def cal_n_kill(self):
-        n = 0
-        last_time = time.time()
-        for kill_time in reversed(self.kill_time_list):
-            if last_time - kill_time <= sfg.Achievement.N_KILL_TIMEDELTA:
-                n += 1
-                last_time = kill_time
-            else:
-                break
-
-        return n
-
-
     def draw(self, camera):
         pass
