211a6725d0b26a8a2531c835a568fc4bbfccada0
busuncle <lizhij2@gmail.com>
2013-05-26 10:54:51 +0800

    comment 2 images

--- etc/setting.py
+++ etc/setting.py
@@ -1310,14 +1310,14 @@ SPRITE_FRAMES = {
 STATIC_OBJECT_IMAGES = ("static_object", {
     "s1": "s1.png",
     "s2": "s2.png", 
-    "s3": "s3.png",
+    #"s3": "s3.png",
     "s4": "s4.png",
     "s5": "s5.png",
     "s6": "s6.png", 
     "s7": "s7.png", 
     "s8": "s8.png",
     "s9": "s9.png",
-    "s10": "s10.png",
+    #"s10": "s10.png",
     "s11": "s11.png",
     "food": "food.png",
 })
